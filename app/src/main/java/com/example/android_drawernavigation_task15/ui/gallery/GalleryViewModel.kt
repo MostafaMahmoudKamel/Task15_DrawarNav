6cab13b40a768af152b86af9d1c51ae040d26b94
MostafaMahmoudKamel <mk4274713@gmail.com>
2024-09-14 13:39:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android_drawernavigation_task15/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.android_drawernavigation_task15.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+
+}
